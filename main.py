f8f6bae0da80674f04959eede833ce2ac2b504a8
Razvan Popa <razvan.a.popa@dxc.com>
2024-11-10 19:25:04 +0200

    Add User Validation and Param Search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import warnings
+warnings.filterwarnings("ignore", category=requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 class Data:
     def __init__(self, field_names, field_values):
@@ -16,27 +18,57 @@ class Request:
         self.response = None
         self.site = site
         self.auth = auth
+        self.num_entries = None
 
-    def getAPIResponse(self,api_url):
-        url = f"{self.site}/{api_url}"
+    def getAPIResponse(self,api_url,params_name=None,params_value=None):
+        if not params_name or not params_value:
+            param_string = ""
+        elif isinstance(params_name, list) and isinstance(params_value, list):
+            if len(params_name) != len(params_value):
+                raise ValueError("Lists do not have the same size!")
+            param_string = "?"
+            for param_index in range(len(params_name)):
+                param_string+= params_name[param_index] + "=" + params_value[param_index] + "&"
+            param_string = param_string[:-1]
+        elif isinstance(params_name, str) and isinstance(params_value,(str,int)):
+            param_string = f"?{params_name}={params_value}"
+        else:
+            raise ValueError("Types are invalid!")
+        url = f"{self.site}/{api_url}{param_string}"
         self.response = requests.get(url, headers=self.auth, verify=False)
         if self.response.status_code != 200:
             print(f"Error! ({self.response.status_code})")
+        self.num_entries = self.response.json()["meta"]["pagination"]["total"]
 
     def postAPIResponse(self,api_url,data):
         url = f"{self.site}/{api_url}"
         self.response = requests.post(url,data=data, headers=self.auth, verify=False)
         if self.response.status_code != 201 and self.response.status_code != 200:
-            print(f"Error! ({self.response.status_code})")
+            print(self.response.json())
+            raise ValueError(f"Error! ({self.response.status_code})")
+
+    def validateResponse(self,api_url):
+        self.getAPIResponse(api_url)
+        url = f"{self.site}/{api_url}"
+        validate_response = requests.get(url, headers=self.auth, verify=False)
+        if validate_response.json()["meta"]["pagination"]["total"] == self.num_entries+1:
+            print("Validation OK!")
+        else:
+            print(f"Expected {self.num_entries+1} but got {validate_response.json()["meta"]["pagination"]["total"]}")
+            raise ValueError("Could not validate response! User already exists or cannot be created.")
 
     def prettifyResponse(self, size = 10):
         if self.response.status_code == 200:
+            meta = self.response.json()["meta"]
+            print("pagination:")
+            for field in meta["pagination"]:
+                print(f"\t-{field}:{meta["pagination"][field]}")
             data = self.response.json()["data"]
-            for data_index in range(size):
-                print("==================================")
+            print("\n data:\n")
+            for data_index in range(min(size,len(data))):
                 for field in data[data_index]:
-                    print(f"{field}:{data[data_index][field]}")
-                print("==================================")
+                    print(f"\t-{field}:{data[data_index][field]}")
+                print('='*40)
 
 
 def main():
@@ -70,8 +102,11 @@ def main():
 
     print("-------------------------New  User----------------------------")
 
-    req_data = Data(["name", "email", "gender"], ["John Doe", "JohnDoe@example.test", "male"])
-    req.postAPIResponse("/users", req_data.data)
+    req_data = Data(["name","email","gender","status"],["John Doe","John_Doe@test.example","male","active"])
+    req.postAPIResponse("/users",req_data.data)
+    req.getAPIResponse("/users", list(req_data.data.keys()), list(req_data.data.values()))
+    req.prettifyResponse()
+    req.validateResponse("/users")
 
 if __name__ == "__main__":
     main()
